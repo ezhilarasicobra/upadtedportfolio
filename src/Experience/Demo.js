b5816fbdcfbb1be37f368b3b7847a64b3236b29d
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-15 18:36:59 +0530

    upad profession

---
+++ src/Experience/Demo.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+
+export default function CustomizedTimeline() {
+  return (
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="h5"
+          color="white"
+        >
+          Feb 2017 - May 2020
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h5"  component="span">
+            Cognizant Technology Solutions - Programmer Analyst.
+          </Typography>
+          <br></br>
+          <Typography variant="h7" >
+            
+            <li>BOT Support - For 30 BOTS in Production.</li>
+            <li>Scrutanizing BOT Perfomance in Production.</li>
+            
+            </Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="h5"
+          color="white"
+        >
+          Sep 2016 - Jan 2017
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h5" component="span">
+           Sutherland Global Solutions - Consultant
+          </Typography>
+          <br></br>
+          <Typography variant='h7'>Worked as chat Person for Amazon UK Region.</Typography>
+          <br></br>
+          <Typography variant='h7'>Guided Users with their Order Information.</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      
+      
+    </Timeline>
+  );
+}
